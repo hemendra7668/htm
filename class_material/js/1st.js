622fb3510f3aa4ad1af164d5e8b63d20aa11779f
Hemendra Kumar <hem.pachahara@gmail.com>
2024-08-13 12:02:55 +0530

    the js started

---
+++ class_material/js/1st.js
@@ -0,0 +1,52 @@
+
+let departmentCourse= "cse"
+departmentCourse = "cse-computer"
+console.log("the daprtment is "+departmentCourse);
+
+
+// const year = 2025;
+//  year=2026;
+
+// console.log(year)
+
+//snakecase for declaring the variable(naming conventions) 
+var  year_month = "january"
+var year_month = "february"
+console.log("month:"+year_month)
+
+
+const val =2*2;
+const Val = 3*2;
+console.log(val)
+console.log(Val)
+
+
+const students=[
+    {
+        
+        "name":"hemendra",
+        "dob" :  new Date("12/12/2012"),
+        "college":"college name",
+        "cpi": 7.8
+    },
+    {
+        
+        "name":"hemendra1",
+        "dob" :  new Date("15/12/2012"),
+        "college":"college name",
+        "cpi": 8.8
+    },
+   
+];
+console.log(students)
+
+const studentname="dear diary"
+let age = 25;
+ var new_name = "dear dear diary"
+
+ const sts={
+    name:studentname,
+    age:age,
+    new_name:new_name
+ }
+ console.log(sts)
